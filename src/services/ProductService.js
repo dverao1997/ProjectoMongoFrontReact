8cbf6f73f2d0aa8bdecbc10bd824c7ed0d055c79
dverao97 <diego.vera.est@tecazuay.edu.ec>
2023-02-25 23:00:02 -0500

    ...

---
+++ src/services/ProductService.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export class ProductService {
+
+    //baseUrl = "http://localhost:8080/api/products/";
+    baseUrl = "https://crudmongojuanyanqui-production.up.railway.app/api/products/";
+    create(product){
+        return axios.post(this.baseUrl+"product", product).then(res => res.data);
+    }
+
+    readAll(){
+        return axios.get(this.baseUrl).then(res => res.data);
+    }
+
+    update(product){
+        return axios.put(this.baseUrl+"product/"+product._id, product).then(res => res.data);
+    }
+
+    delete(id){
+        return axios.delete(this.baseUrl+"product/"+id).then(res => res.data);
+    }
+}
+
+
